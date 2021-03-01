e5d91ffd5166939580fb6366e6372c1faea7f3c7
Pedro Provenzano <pedroprov0@gmail.com>
2021-03-01 19:54:07 -0300

    add send message

--- Scripts/contacto.js
+++ Scripts/contacto.js
@@ -29,23 +29,72 @@ botonFormCancelar.addEventListener("click", () => {
 });
 
 botonFormEnviar.addEventListener("click", () => {
-  // Fetch POST formulario
-
-  // if ok
-
-  botonFormEnviar.innerText = "ENVIADO!";
-  botonFormEnviar.style.color = "#0fff2f";
-  setTimeout(() => {
-    sectionForm.style.opacity = "0%";
+  if (
+    inputNombre.value == "" ||
+    inputApellido.value == "" ||
+    inputEmail.value == "" ||
+    inputTextArea.value == ""
+  ) {
+    botonFormEnviar.innerText = "ERROR!";
+    botonFormEnviar.style.color = "#ff0f0f";
     setTimeout(() => {
-      sectionForm.style.display = "none";
-      botonContacto.style.color = "black";
       botonFormEnviar.innerText = "ENVIAR";
       botonFormEnviar.style.color = "white";
-      inputNombre.value = "";
-      inputApellido.value = "";
-      inputEmail.value = "";
-      inputTextArea.value = "";
-    }, 500);
-  }, 1000 * 2);
+    }, 1000 * 2);
+    return;
+  }
+
+  let bodyCont = {
+    nombre: `${inputNombre.value} ${inputApellido.value}`,
+    email: `${inputEmail.value}`,
+    contenido: `${inputTextArea.value}`,
+  };
+
+  // Fetch POST formulario
+  fetch("https://lasilla-api.herokuapp.com/mensajes/enviar", {
+    method: "POST",
+    body: JSON.stringify(bodyCont),
+    headers: {
+      connection: "keep-alive",
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+      accept: "*/*",
+      "sec-fetch-site": "same-origin",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-dest": "empty",
+      referer: "https://lasilla-api.herokuapp.com/mensajes/enviar",
+      "accept-language": "es-US,es-419;q=0.9,es;q=0.8,en;q=0.7",
+      "content-type": "application/json",
+      host: "https://lasilla-api.herokuapp.com",
+      "accept-encoding": "gzip, deflate, br",
+      "content-length": "84",
+    },
+  })
+    .then((res) => res.text())
+    .then((res) => {
+      if (res == "Mensaje enviado correctamente") {
+        botonFormEnviar.innerText = "ENVIADO!";
+        botonFormEnviar.style.color = "#0fff2f";
+        setTimeout(() => {
+          sectionForm.style.opacity = "0%";
+          setTimeout(() => {
+            sectionForm.style.display = "none";
+            botonContacto.style.color = "black";
+            botonFormEnviar.innerText = "ENVIAR";
+            botonFormEnviar.style.color = "white";
+            inputNombre.value = "";
+            inputApellido.value = "";
+            inputEmail.value = "";
+            inputTextArea.value = "";
+          }, 500);
+        }, 1000 * 2);
+      } else {
+        botonFormEnviar.innerText = "ERROR!";
+        botonFormEnviar.style.color = "#ff0f0f";
+        setTimeout(() => {
+          botonFormEnviar.innerText = "ENVIAR";
+          botonFormEnviar.style.color = "white";
+        }, 1000 * 2);
+      }
+    });
 });
